e1002fbcbf3422966fd8babd213bbfe4720efa49
anhyeongkwan <bombom841@naver.com>
2023-03-29 01:57:14 +0900

    - 프로그래머스 퀴즈, 자릿수 합, 배수

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import programmers.Programmer1;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
@@ -20,5 +22,22 @@ public class Main {
         Programmer1 programmer12 = new Programmer1();
         System.out.println(programmer12.contain("ab6CDE443fgh22iJKlmn1o", "6CD"));
 
+        System.out.println();
+
+        System.out.println("사칙연산");
+        Programmer1 programmer13 = new Programmer1();
+        System.out.println(Arrays.toString(programmer13.quiz(new String[]{"19 - 6 = 13", "5 + 66 = 71", "5 - 15 = 63", "3 - 1 = 2"})));
+
+        System.out.println();
+
+        System.out.println("자릿수 더하기");
+        Programmer1 programmer14 = new Programmer1();
+        System.out.println(programmer14.placeSuM(1234));
+
+        System.out.println();
+
+        System.out.println("배수 구하기");
+        Programmer1 programmer15 = new Programmer1();
+        System.out.println(Arrays.toString(programmer15.multiple(5, new int[]{1, 9, 3, 10, 13, 5})));
     }
 }
--- src/programmers/Programmer1.java
+++ src/programmers/Programmer1.java
@@ -1,5 +1,8 @@
 package programmers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Programmer1 {
 
     // 어떤 세균은 1시간에 두배만큼 증식한다고 합니다.
@@ -50,9 +53,58 @@ public class Programmer1 {
     //덧셈, 뺄셈 수식들이 'X [연산자] Y = Z' 형태로 들어있는 문자열 배열 quiz가 매개변수로 주어집니다.
     // 수식이 옳다면 "O"를 틀리다면 "X"를 순서대로 담은 배열을 return하도록 solution 함수를 완성해주세요.
     public String[] quiz(String[] quiz) {
-        String[] answer = {};
+        String[] answer = new String[quiz.length];
+        int idx = 0;
+//        char[] oper = {'+', '-', '*', '/'};
 
+        for (int i = 0; i < quiz.length; i++) {
+            String[] arr = quiz[i].split(" ");
+            int value = (arr[1].equals("+")) ? Integer.parseInt(arr[2]) : Integer.parseInt(arr[2]) * -1;
+            int result = Integer.parseInt(arr[0]) + value;
+
+            System.out.println("계산값 : " + result);
+
+            answer[i] = result == Integer.parseInt(arr[4]) ? "O" : "X";
+
+        }
 
         return answer;
+    }
+
+    //정수 n이 매개변수로 주어질 때 n의 각 자리 숫자의 합을 return하도록 solution 함수를 완성해주세요.
+    public int placeSuM(int n) {
+        int answer = 0;
+
+        String place = String.valueOf(n);
+        System.out.println(Arrays.toString(place.split("")));
+        char[] placeArr = place.toCharArray();
+
+        for (int i = 0; i < place.length(); i++) {
+            answer += Character.getNumericValue(placeArr[i]); // 문자를 숫자로 변환하는 함수(아스키코드에서 - '0'을 해도 된다.)
+        }
+
+        return answer;
+    }
+
+    //정수 n과 정수 배열 numlist가 매개변수로 주어질 때,
+    // numlist에서 n의 배수가 아닌 수들을 제거한 배열을 return하도록 solution 함수를 완성해주세요.
+    public int[] multiple(int n, int[] numList) {
+
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        for (int i = 0; i < numList.length; i++) {
+            if (numList[i] % n != 0) {
+                continue;
+            }
+            list.add(numList[i]);
+        }
+
+        int[] result = new int[list.size()];
+
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
+
+        return result;
     }
 }
